9278009d956b0de6f5a7a9635831f9f7f1054bbe
Ken <saganawski@gmail.com>
2024-09-10 10:43:44 -0400

    solve jewels and stones problem on leetcode

---
+++ src/main/java/com/leetcode/leetcode/JewelsAndStones.java
@@ -0,0 +1,41 @@
+package com.leetcode.leetcode;
+
+import java.util.HashMap;
+
+public class JewelsAndStones {
+    public static void main(String[] args) {
+        final String J = "aA";
+        final String S = "aAAbbbb";
+        final int result = numJewelsInStones(J, S);
+        System.out.println(result);
+    }
+
+    private static int numJewelsInStones(String jewels, String stones) {
+        /*
+        * 1) create a counter to keep track of the number of jewels
+        * 2) loop through the jewels creating a hashMap
+        * 3) loop through the stones and check if the stone is in the hashMap increment the counter
+        * 4) return the counter
+        * */
+
+        int counter = 0;
+
+        if(jewels.length() == 0 || stones.length() == 0){
+            return 0;
+        }
+
+        final HashMap<Character, Integer> jewelMap = new HashMap<>();
+        for (int i = 0; i < jewels.length(); i++) {
+            jewelMap.put(jewels.charAt(i), 0);
+        }
+
+        for (int i = 0; i < stones.length(); i++) {
+            if(jewelMap.containsKey(stones.charAt(i))){
+                counter++;
+            }
+        }
+
+        return  counter;
+
+    }
+}

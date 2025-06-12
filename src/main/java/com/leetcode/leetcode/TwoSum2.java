4f7437a3bc0eb3dcb6e08bafc092afcdaff8255a
Ken Saganski <saganawski@gmail.com>
2025-06-12 17:55:39 -0400

    solve two sum problem

---
+++ src/main/java/com/leetcode/leetcode/TwoSum2.java
@@ -0,0 +1,72 @@
+package com.leetcode.leetcode;
+
+
+import java.util.Arrays;
+
+public class TwoSum2 {
+    public static void main(String[] args) {
+        final int[] given = new int[]{2,7,11,15};
+        final int target = 9;
+
+        final int[] result = twoSum(given,target);
+        System.out.println(Arrays.toString(result));
+    }
+
+    private static int[] twoSum(int[] numbers, int target) {
+        final int[] result = new int[2];
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i +1; j < numbers.length; j++) {
+                int currentSum = numbers[i] + numbers[j];
+                if(currentSum == target){
+                    result[0] = i+1;
+                    result[1] = j+1;
+                    return result;
+                }
+            }
+
+        }
+
+        return  null;
+    }
+}
+/*
+167. Two Sum II - Input Array Is Sorted
+Medium
+        Topics
+premium lock icon
+        Companies
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+
+
+
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+Example 2:
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+Example 3:
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+
+
+Constraints:
+
+        2 <= numbers.length <= 3 * 104
+        -1000 <= numbers[i] <= 1000
+numbers is sorted in non-decreasing order.
+        -1000 <= target <= 1000
+The tests are generated such that there is exactly one solution.*/
